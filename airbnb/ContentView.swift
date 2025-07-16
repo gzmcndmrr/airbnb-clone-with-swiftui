80ef87e3c9cb760abb693d30d2ce011bbe408c98
Gizem Candemir <gizem.candemir@commencis.com>
2025-07-16 17:23:28 +0300

    Initial Commit

---
+++ airbnb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  airbnb
+//
+//  Created by Gizem Candemir on 7/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ airbnb/airbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  airbnbApp.swift
+//  airbnb
+//
+//  Created by Gizem Candemir on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct airbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
